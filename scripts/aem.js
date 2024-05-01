412bfaeec167db3ab815404d9a2c9ea1488ef627
KP Parrish <krparrish@adobe.com>
2024-05-01 17:19:49 -0600

    fixed icons color and color for hover effects

--- scripts/aem.js
+++ scripts/aem.js
@@ -464,6 +464,7 @@ function decorateIcon(span, prefix = '', alt = '') {
     .substring(5);
   const img = document.createElement('img');
   img.dataset.iconName = iconName;
+  img.className = 'svg';
   img.src = `${window.hlx.codeBasePath}${prefix}/icons/${iconName}.svg`;
   img.alt = alt;
   img.loading = 'lazy';
