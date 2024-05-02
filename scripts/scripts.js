3a778813896ca562e248ad1993ffb035e5445525
KP Parrish <krparrish@adobe.com>
2024-05-01 18:11:42 -0600

    finalized card styles, added hero styles, removed section padding to hide empty sections

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -22,10 +22,18 @@ const LCP_BLOCKS = []; // add your LCP blocks to the list
 function buildHeroBlock(main) {
   const h1 = main.querySelector('h1');
   const picture = main.querySelector('picture');
+  const subheading = main.querySelector('h4');
+  const heroCopy = main.querySelector('h5');
+  const heroTextWrapper = document.createElement('div');
+  heroTextWrapper.className = 'hero-text-wrapper';
+  heroTextWrapper.append(subheading);
+  heroTextWrapper.append(h1);
+  heroTextWrapper.append(heroCopy);
+
   // eslint-disable-next-line no-bitwise
   if (h1 && picture && (h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING)) {
     const section = document.createElement('div');
-    section.append(buildBlock('hero', { elems: [picture, h1] }));
+    section.append(buildBlock('hero', { elems: [picture, heroTextWrapper] }));
     main.prepend(section);
   }
 }
